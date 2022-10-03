8bb45d585f0acf060e8d1932312f4e5777a3a293
Phương Đông [Dev_] <phuongdong.dev@gmail.com>
2022-10-03 09:30:08 +0700

    update process env type def

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import { app } from "./app";
 import "dotenv/config";
-const port = process.env["PORT"];
+const port = process.env["PORT"] || 3000;
 app.listen(port, () => {
     console.log("Listening on port", port);
 });
